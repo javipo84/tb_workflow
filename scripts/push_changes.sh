5a7312219bc545b04889b0ba607ad5bf970825ef
javipo84 <javipo84@gmail.com>
2022-03-29 16:43:30 +0200

    test 16

--- scripts/push_changes.sh
+++ scripts/push_changes.sh
@@ -2,6 +2,11 @@ export API_ENDPOINT="https://api.tinybird.co"
 #export TB_TOKEN="p.eyJ1IjogIjk2MTEwOTNhLWJkM2UtNGI0ZS1hZTZlLTc1OWY3NDNmZDg3NyIsICJpZCI6ICIwZjhhNjc1Mi1iM2EwLTQ3YWUtYTUzMi1lZGI2MTliZWYyMjQifQ.XuXn-xTQ5g92p3wqL6Pq7xNXqq-WKhf7PQhGVVOX-68"
 echo "pushing changes"
 
+git fetch origin ${{ github.event.before }} --depth=1
+export DIFF=$( git diff --name-only ${{ github.event.before }} $GITHUB_SHA )
+echo "Diff between ${{ github.event.before }} and $GITHUB_SHA"
+echo "$DIFF"
+
 for file in $(git diff --name-only); do \
     echo $file;
     if [[ $file == *.pipe ]]
